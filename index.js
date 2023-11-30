8f023aa0effd5968f43cba261101d8609ffe30d5
Justinkuo1053 <justinkuo719@gmail.com>
2023-11-30 11:46:55 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const authRoute = require("./routes").auth;
+const courseRoute = require("./routes").course;
+const passport = require("passport");
+require("./config/passport")(passport);
+const cors = require("cors");
+
+mongoose
+  .connect("mongodb://127.0.0.1/mernDB")
+  .then(() => {
+    console.log("連結到mongodb...");
+  })
+  .catch((e) => {
+    return res.status(500).send("無法儲存使用者。。。");
+    return console.log(e);
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/api/user", authRoute);
+
+app.use(
+  "/api/courses",
+  passport.authenticate("jwt", { session: false }),
+  courseRoute
+);
+
+app.listen(8080, () => {
+  console.log("後端伺服器聆聽在port 8080...");
+});
